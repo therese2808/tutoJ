9a3f6c90e1f03be140cb1f7a24aa5912c6d4e35b
therese2808 <therese.wem@yahoo.com>
2023-10-07 21:23:20 +0100

    rattrapage

---
+++ scriptrattrapage.js
@@ -0,0 +1,24 @@
+var age= 25 //declaration de variable+ instantiation + redeclaration et qui dit redeclaration dit aussi reinstantiatoiion
+console.log(age);
+var age =30
+console.log(age);
+let prenom='Mercy'//declaration de variable+ instantiation+ reinstantier
+prenom= 'Serge'
+console.log(prenom);
+const nom ='therese' //const on donne une valeur ie on peut instantier la variable mais pas la déclarer
+console.log(nom);
+
+// Opération mathématique sur des variables
+
+var number1 = 2
+let number2 = 10
+var resultat = number1 + number2
+console.log('Op Addition 1er forme: '+resultat);
+
+number1+=number2
+console.log('Op Addition 2e forme: '+number1);
+
+// var reste= number2%number1
+// console.log('Reste de la division: ' +reste);
+
+console.log('Type de variable de Resultat est ' + typeof resultat);
