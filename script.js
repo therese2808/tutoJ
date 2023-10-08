c40d7bf01e197591879f6fc37320c2cb7b59e2b5
therese2808 <therese.wem@yahoo.com>
2023-10-08 21:51:44 +0100

    mise jour

--- script.js
+++ script.js
@@ -1,241 +1,262 @@
-// //voici mon code js 
-// var nom= 'mercy';// declaration
-// nom ='Olivier';//instiation
-// var nom = 'Apotre';//redeclaration
-// console.log('le nom est :');
-// console.log(nom);
-// let prenom = 'ali'; //declaration
-// console.log('le prenom est :');
-// prenom = 'Fermat';
-// console.log(prenom);
-// const age=20;
-// console.log('l age est :');
-// console.log(age);
+//voici mon code js 
+var nom= 'mercy';// declaration
+nom ='Olivier';//instiation
+var nom = 'Apotre';//redeclaration
+console.log('le nom est :');
+console.log(nom);
+let prenom = 'ali'; //declaration
+console.log('le prenom est :');
+prenom = 'Fermat';
+console.log(prenom);
+const age=20;
+console.log('l age est :');
+console.log(age);
 
-// var bool = true;
+var bool = true;
 
-// // Operation sur les variables
+// Operation sur les variables
 
-// var number1= 10;
-// var number2= 5;
+var number1= 10;
+var number2= 5;
 
-// // addition 
-// console.log("Addition");
-// console.log(number1+number2);
+// addition 
+console.log("Addition");
+console.log(number1+number2);
 
-// // Soustration 
-// console.log("Soustration");
-// console.log(number1-number2);
+// Soustration 
+console.log("Soustration");
+console.log(number1-number2);
 
-// // Multiplication 
-// console.log("Multiplication");
-// console.log(number1*number2);
+// Multiplication 
+console.log("Multiplication");
+console.log(number1*number2);
 
-// // Division 
-// console.log("Division");
-// console.log(number1/number2);
+// Division 
+console.log("Division");
+console.log(number1/number2);
 
-// // Modulo 
-// console.log("Modulo");
-// console.log(number1%number2);
+// Modulo 
+console.log("Modulo");
+console.log(number1%number2);
 
-// // Exponentielle 
-// console.log("Exponentielle");
-// console.log(number1**number2);
+// Exponentielle 
+console.log("Exponentielle");
+console.log(number1**number2);
 
-// //affectation
+//affectation
 
-// number1+=number2
-// console.log(`affectation `+number1);
+number1+=number2
+console.log(`affectation `+number1);
 
-// // incrementation
-// var i=0
-// i++;
-// console.log('incrementation '+i);
-// i=10
-// i--;
-// console.log('décrementation '+i);
+// incrementation
+var i=0
+i++;
+console.log('incrementation '+i);
+i=10
+i--;
+console.log('décrementation '+i);
 
-// // Type de variable
-// console.log('Type de variable '+number1);
+// Type de variable
+console.log('Type de variable '+number1);
 
-// // declaration objet
+// declaration objet
 
-// var Etudiant= new Object();
-// Etudiant={ prenom:0.0, email:'a@a.com'};
-// Etudiant.nom='Mercy';
-// Etudiant.age=25;
-// Etudiant.sexe='f'
-// Etudiant.prenom = Boolean(Etudiant.prenom);
-// console.log("Declaration des objets");
-// console.log(Etudiant);
+var Etudiant= new Object();
+Etudiant={ prenom:0.0, email:'a@a.com'};
+Etudiant.nom='Mercy';
+Etudiant.age=25;
+Etudiant.sexe='f'
+Etudiant.prenom = Boolean(Etudiant.prenom);
+console.log("Declaration des objets");
+console.log(Etudiant);
 
-// console.log("Type des champs");
-// console.log(typeof(Etudiant.prenom));
+console.log("Type des champs");
+console.log(typeof(Etudiant.prenom));
 
-// function etudes(){
-//     var voiture= new Object();
-//     function mecanique(){
-//         console.log('type voiture dans la fonction '+typeof (voiture));
-//     }
-//     mecanique();
-// }
-// etudes();
-// // ici il y'aura une eurreur car voiture est propre à la fonction etude
-// //console.log(voiture);
+function etudes(){
+    var voiture= new Object();
+    function mecanique(){
+        console.log('type voiture dans la fonction '+typeof (voiture));
+    }
+    mecanique();
+}
+etudes();
+// ici il y'aura une eurreur car voiture est propre à la fonction etude
+//console.log(voiture);
 
-// prenom= new Object();
-// var toto='tarta';
-// console.log(toto);
-// prenom= { nom: toto};
+prenom= new Object();
+var toto='tarta';
+console.log(toto);
+prenom= { nom: toto};
 
-// f(prenom);
-// function f(Object){
-//     console.log('test');
-// }
+f(prenom);
+function f(Object){
+    console.log('test');
+}
 
-// // taille des chaines de caractères
-// var prof ="true";
-// console.log('taille des caractères'+prof.length);
-// console.log(prof[1]);
-// console.log(typeof(prof.toString()));
-// var x= eval(3/4)
-// console.log(typeof x);
-// var test= 'Paul est un professeur \n'
-// +prof
-// console.log(test);
+// taille des chaines de caractères
+var prof ="true";
+console.log('taille des caractères'+prof.length);
+console.log(prof[1]);
+console.log(typeof(prof.toString()));
+var x= eval(3/4)
+console.log(typeof x);
+var test= 'Paul est un professeur \n'
++prof
+console.log(test);
 
-// //Tableaux
-// // Declaration tableau
-// var employe = []
-// console.log(`Declaraion crochet `+typeof employe);
-// var employe = ["employé1","emloyé2","employé3"]
-// console.log(`Declaraion instation `+  employe);
-// //x var employe = new Array()
-// // console.log(`Declaraion new array `+typeof employe);
+//Tableaux
+// Declaration tableau
+var employe = []
+console.log(`Declaraion crochet `+typeof employe);
+var employe = ["employé1","emloyé2","employé3"]
+console.log(`Declaraion instation `+  employe);
+//x var employe = new Array()
+// console.log(`Declaraion new array `+typeof employe);
 
-// // manipulation array
-// console.log(employe[0]);
-// //taile d'un array
-// console.log(employe.length);
-// //que peut on stocker dans un array
-// let array= ["etudiant",true, function testEtudiant(){
-//     console.log(employe);
-// }]
-// console.log(typeof array[2]);
-// //ajout dans array à la derniere ligne
-// array.push('etudes')
-// console.log(array);
+// manipulation array
+console.log(employe[0]);
+//taile d'un array
+console.log(employe.length);
+//que peut on stocker dans un array
+let array= ["etudiant",true, function testEtudiant(){
+    console.log(employe);
+}]
+console.log(typeof array[2]);
+//ajout dans array à la derniere ligne
+array.push('etudes')
+console.log(array);
 
-// //Supprimer dans un tableau à la dernière ligne
-// array.pop()
-// console.log(array);
+//Supprimer dans un tableau à la dernière ligne
+array.pop()
+console.log(array);
 
-// //ajout dans array à la première ligne
+//ajout dans array à la première ligne
 
-// console.log(array.unshift('therese','olivier', 'apotre'));
-// console.log(array);
+console.log(array.unshift('therese','olivier', 'apotre'));
+console.log(array);
 
-// //Supprimer dans un tableau à la première ligne
-// console.log(array.shift())
-// console.log(array);
+//Supprimer dans un tableau à la première ligne
+console.log(array.shift())
+console.log(array);
 
-// //Utilisation de map
+//Utilisation de map
 
-// let array1= array.map(x=>x+3)
-// console.log(array1);
+let array1= array.map(x=>x+3)
+console.log(array1);
 
-// //Structure repetive et conditionnelle
+// utilisation de set
 
-// var toto= 2
-// var tata =2
-// // boucle if
-// if(tata!==toto)// je n'ai pmas mis les crochet car je n'ai plus de 2 lignes
-// console.log('comparaison reussie');
+var tab= [1,2,3,4,5]
+var tabSet= new Set(tab)
+console.log(`declaration tableau ${tab}
+             declaration set vide ${tabSet.has(8)}`);
+             //ajouter dans n set
+tabSet.add('Ali')
+tabSet.delete('Ali')
+for (const item of tabSet.values()) {
+console.log(item);
+}
+// taille d'un set
+console.log(`taille du set ${tabSet.size}`);
+//set operation
+let set1= new Set([1,2,3,9,8])
+const intersection = new Set([...tabSet].filter((x) => set1.has(x)));
+console.log(`filtrage d'un set ${intersection}`);
+for (const item of intersection.values()) {
+    console.log(`filtre ${item}`);
+    }
+//Structure repetive et conditionnelle
 
-// // boucle iifelse
-// if(tata===toto)
-// console.log('sont similaires');
-// else
-// console.log('sont differents');
-// //boucle if else if
-// if(tata==toto)
-// console.log('sont similaires');
-// else if(toto!=tata)
-// console.log('sont differents');
-// else
-// console.log(typeof(toto)==typeof tata);
-// console.log(`N'ont pas le même type`);
+var toto= 2
+var tata =2
+// boucle if
+if(tata!==toto)// je n'ai pmas mis les crochet car je n'ai plus de 2 lignes
+console.log('comparaison reussie');
 
-// Etudiant = {nom:'Appotre', prenom:'therese', solde:"test"}
-// // Switch
-// switch (Etudiant.solde) {
-//     case 'paye':
-//         document.write('Etudiant a paye')
-//         break;
-//     case 'impaye':
-//         document.write(`Etudiant n'a pas paye`)
-//         break;
-//     case 'inscrit':
-//     document.write(`Etudiant est inscrit`)
-//     break;
-//     default:
-//         document.write('Potentiel etudiant')
-//         break;
-// }
-// //Boucle ternaire
-// (tata===toto)?console.log('sont similaires'):console.log('sont differents');
+// boucle iifelse
+if(tata===toto)
+console.log('sont similaires');
+else
+console.log('sont differents');
+//boucle if else if
+if(tata==toto)
+console.log('sont similaires');
+else if(toto!=tata)
+console.log('sont differents');
+else
+console.log(typeof(toto)==typeof tata);
+console.log(`N'ont pas le même type`);
 
-// // Boucle For
-// // avec l'incrementation
-// tab=[Etudiant]
-// tab[0]= {nom:'Appotre', prenom:'therese', solde:"test"}
-// tab[1]={nom:'Ali', prenom:'olivier', solde:"paye"}
-// tab[2]={nom:'mercy', prenom:'serge', solde:"impaye"}
-// for(let i = 0;i>tab.length;i++){
-//     console.log(tab[i]);
-// }
-// // avec decrementation
-// for(let i = tab.length;i>=0;i--){
-//     console.log(tab[i]);
-// }
-// // boucle while
-// n= 0
-// while (n<3) {
-//     n++;
-//     console.log(n);
+Etudiant = {nom:'Appotre', prenom:'therese', solde:"test"}
+// Switch
+switch (Etudiant.solde) {
+    case 'paye':
+        document.write('Etudiant a paye')
+        break;
+    case 'impaye':
+        document.write(`Etudiant n'a pas paye`)
+        break;
+    case 'inscrit':
+    document.write(`Etudiant est inscrit`)
+    break;
+    default:
+        document.write('Potentiel etudiant')
+        break;
+}
+//Boucle ternaire
+(tata===toto)?console.log('sont similaires'):console.log('sont differents');
 
-// }
-// Etudiant ={nom:'th', prenom:'test', age:22}
-// //fonction
-// /*function etudiantfonction(Etudiant){
-//     var concition
-//    (Etudiant.age<20)? concition= true:concition=false
-//    return concition
-// }*/
-// // 2e facon d'ecrire la fonction etutiantfonction
-// function etudiantfonction(Etudiant){
-//     if(Etudiant.age<20)
-//     return true
-// else return false
-// }
+// Boucle For
+// avec l'incrementation
+tab=[Etudiant]
+tab[0]= {nom:'Appotre', prenom:'therese', solde:"test"}
+tab[1]={nom:'Ali', prenom:'olivier', solde:"paye"}
+tab[2]={nom:'mercy', prenom:'serge', solde:"impaye"}
+for(let i = 0;i>tab.length;i++){
+    console.log(tab[i]);
+}
+// avec decrementation
+for(let i = tab.length;i>=0;i--){
+    console.log(tab[i]);
+}
+// boucle while
+n= 0
+while (n<3) {
+    n++;
+    console.log(n);
 
-// function afficheEtudiant(Etudiant) {
-//     console.log(Etudiant.nom +' ' +
-//         Etudiant.prenom +
-//         Etudiant.age)
+}
+Etudiant ={nom:'th', prenom:'test', age:22}
+//fonction
+/*function etudiantfonction(Etudiant){
+    var concition
+   (Etudiant.age<20)? concition= true:concition=false
+   return concition
+}*/
+// 2e facon d'ecrire la fonction etutiantfonction
+function etudiantfonction(Etudiant){
+    if(Etudiant.age<20)
+    return true
+else return false
+}
 
-// }
-// afficheEtudiant(Etudiant)
-// var condition=etudiantfonction(Etudiant)
-// console.log(condition);
-// var num= 5
-// // exeption
-// try {
-//    if (num%2!=0)
-//     throw new Error ('Nombre impair')
-// } catch (error) {
-//     console.log(error);
-// }finally{
-//     console.log('Exception executée');
-// }
+function afficheEtudiant(Etudiant) {
+    console.log(Etudiant.nom +' ' +
+        Etudiant.prenom +
+        Etudiant.age)
+
+}
+afficheEtudiant(Etudiant)
+var condition=etudiantfonction(Etudiant)
+console.log(condition);
+var num= 5
+// exeption
+try {
+   if (num%2!=0)
+    throw new Error ('Nombre impair')
+} catch (error) {
+    console.log(error);
+}finally{
+    console.log('Exception executée');
+}
--- scriptrattrapage.js
+++ scriptrattrapage.js
@@ -20,5 +20,35 @@ console.log('Op Addition 2e forme: '+number1);
 
 // var reste= number2%number1
 // console.log('Reste de la division: ' +reste);
-
+// Type de variable en js avec Typeof
 console.log('Type de variable de Resultat est ' + typeof resultat);
+var var1= null
+var var2
+var var3=20
+var var4 = new Object()
+var var5='bonjour'
+var var6= true
+
+console.log(`type var1 normalement null et js dit quoi `+ typeof var1 );
+console.log(`type var2 normalement undefined et js dit quoi `+ typeof var2 );
+console.log(`type var3 normalement number et js dit quoi `+ typeof var3 );
+console.log(`type var4 normalement Object et js dit quoi `+ typeof var4 );
+console.log(`type var5 normalement String et js dit quoi `+ typeof var5 );
+console.log(`type var6 normalement boolean et js dit quoi `+ typeof var6 );
+
+// Conversion de type
+//Premiere methode pour String
+var2=String(var2)
+console.log('Valeur de var2 '+var2);
+console.log('Conversion de var2 '+typeof var2);
+// 2e methode pour string
+console.log(`conversion meth2  `+ var2.toString());
+console.log(`conversion meth2 type `+typeof var2.toString());
+// conversion en majuscule
+console.log('Passer de miniscule en majuscule '+var5.toUpperCase());
+console.log('taille de mon caractère '+ var5.length);
+//utilisation de template string
+console.log(`Bienvenue au cours de logone
+              ${var5} Mercy`);
+// vs methode normale
+console.log('Bienvenue au cours de logone\n'+var5+'           Mercy');
